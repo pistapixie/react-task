2b1882e86f201b7508fc8e03280296c4339198a6
pistapixie <hleehslee@gmail.com>
2024-01-29 03:29:35 +0900

    리스트 랜더링 및 데이터 바인딩 구현

---
+++ week02/src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import "./Button.css";
+
+function Button() {
+  return (
+    <button type="submit" className="category-submit paragragh-s">
+      이대로 저장할래요
+    </button>
+  );
+}
+
+export default Button;
---
+++ week02/src/components/CategoryCard/CategoryCard.jsx
@@ -0,0 +1,28 @@
+import "./CategoryCard.css";
+import plus from "/src/assets/plus.svg";
+import categoryData from "../../data/category.json";
+
+console.log(categoryData.items);
+
+export function CategoryCard() {
+  const { items } = categoryData;
+  const result = items.map(({ id, mainCategory, subCategory }) => {
+    return (
+      <li key={id}>
+        <button type="button" className="category-card">
+          <div className="category-name">
+            <p className="paragraph-s">{mainCategory}</p>
+            <p className="heading-s">{subCategory}</p>
+          </div>
+          <img src={plus} alt="관심분야에 추가" />
+        </button>
+      </li>
+    );
+  });
+
+  return <ul className="category-list">{result}</ul>;
+}
+
+export function CategoryCardList({ children }) {
+  return <ul className="category-list">{children}</ul>;
+}
